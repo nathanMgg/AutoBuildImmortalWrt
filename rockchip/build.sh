60bcd389d7ad7cb6b188218db173780a213d30b9
Nathan.mgg <cm.cheung321@gmail.com>
2025-07-28 11:39:49 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -64,7 +64,7 @@ PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 # docker
-PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
