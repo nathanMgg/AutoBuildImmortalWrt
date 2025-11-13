194f683c297dd3ceb262d678894c40403ced2d77
Nathan.mgg <cm.cheung321@gmail.com>
2025-11-13 10:34:17 +0800

    Update build-rockchip-immortalWrt-24.10.2.yml

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -60,6 +60,8 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
 
 # 第三方软件包 合并
 # ======== shell/custom-packages.sh =======
